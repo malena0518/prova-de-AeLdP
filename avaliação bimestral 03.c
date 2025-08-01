8c79f6f2c98bd1c3538335331a42f58f7aa9da22
Helenaa <helenasoares18062014@gmail.com>
2025-08-01 08:42:06 -0300

    Update avaliação bimestral 03.c

--- avaliação bimestral 03.c
+++ avaliação bimestral 03.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+//Escreva um programa que solicite ao usuario a idade, o montante a depositar e o numero de conta em que se quer realizar o deposito, declarando as variaveis  como short, int, long.
+int main() {
+    short idade;
+    int montante;
+    long numero_conta;
+
+    printf("digite sua idade: ");
+    scanf("%hd", &idade);
+
+    printf("informe o montante a depositar: ");
+    scanf("%d", &montante);
+
+    printf("informe o número da conta: ");
+    scanf("%ld", &numero_conta);
+
+    printf("\nOperação:\n");
+    printf("Idade: %hd anos\n", idade);
+    printf("Montante a depositar: R$ %d\n", montante);
+    printf("Número da conta: %ld\n", numero_conta);
+
+    return 0;
+}
 
